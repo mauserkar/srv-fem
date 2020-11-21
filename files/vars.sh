cbcf1860719d26264b109269063b32e63078d96d
Carlos Garcia <c.garcia@whisbi.com>
2020-11-21 01:42:50 +0100

    add vars

---
+++ files/vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ENV_REPO_PATH="/home/$USER/srv-fem"
+export ENV_MYSQL_NAME="example_name"
+export ENV_MYSQL_DB="example_db"
+export ENV_MYSQL_USER="example_user"
+export ENV_MYSQL_PASSWORD="example_pass"
