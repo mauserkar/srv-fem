3d85656dc4aceac988992f49a33482c85ca2a4a7
Carlos Garcia <carlos.garcia@scrm.eu>
2020-11-27 12:12:38 +0100

    add swarm stack

--- files/example-secrets.sh
+++ files/example-secrets.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-
-export ENV_REPO_PATH="/home/$USER/srv-fem"
-export ENV_MYSQL_NAME=""
-export ENV_MYSQL_DB=""
+###################### Variables ###########################
+# Common
+export ENV_USER="$USER"
+export ENV_REPO_PATH="/home/$ENV_USER/repos/srv-fem"
+# Mysql
+export ENV_MYSQL_SERVER=""
+export ENV_MYSQL_DB_DEV=""
+export ENV_MYSQL_DB_PRO=""
 export ENV_MYSQL_USER=""
 export ENV_MYSQL_PASSWORD=""
+# Wordpress
 export ENV_WORDPRESS_DB_HOST=""
 export ENV_WORDPRESS_DB_NAME=""
 export ENV_WORDPRESS_DB_USER=""
 export ENV_WORDPRESS_DB_PASSWORD=""
+############################################################
+###################### Alias ###############################
+alias env-vars="source /home/$USER/repos/srv-fem/files/secrets.sh"
 
-echo "usage: envsubst < file_with_env_vars.yml | kubectl create -f -"
+
+
+############################################################
+## View variables
+echo "Variables:"
+env | grep "ENV_"
+# View Alias
+alias
+
+# Usage:
+echo "usage: envsubst < file_with_env_vars.yml | command -"
