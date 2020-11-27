fb13e0ee4c15c9b6fe182a3d9f4b9c7e4b29713c
Carlos Garcia <carlos.garcia@scrm.eu>
2020-11-27 22:30:40 +0100

    update gitignore and update tmpl

--- files/example-secrets.sh
+++ files/example-secrets.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
-###################### Variables ###########################
+###################### Secrets Vars ########################
 # Common
 export ENV_USER="$USER"
-export ENV_REPO_PATH="/home/$ENV_USER/repos/srv-fem"
+export ENV_REPO_PATH=""
+export ENV_SECRETS_FILE=""
+
 # Mysql
 export ENV_MYSQL_SERVER=""
 export ENV_MYSQL_USER=""
 export ENV_MYSQL_PASSWORD=""
+
 # Wordpress
 export ENV_WORDPRESS_DB_DEV=""
 export ENV_WORDPRESS_DB_PRD=""
 export ENV_WORDPRESS_USER=""
 export ENV_WORDPRESS_PASSWORD=""
-############################################################
+
+# NextCloud
+export ENV_NEXTCLOUD_DB=""
+export ENV_NEXTCLOUD_USER=""
+export ENV_NEXTCLOUD_PASSWORD=""
+
 ###################### Alias ###############################
-alias env-vars="source /home/$USER/repos/srv-fem/files/secrets.sh"
-
-
+alias env_load_vars="source $ENV_REPO_PATH/files/$ENV_SECRETS_FILE"
 
 ############################################################
+##################### Common ###############################
+
 ## View variables
-echo "Variables:"
-env | grep "ENV_"
+echo "--- Variables ---"
+env | grep --color=never "ENV_"
+
 # View Alias
-alias
+echo "--- Alias ---"
+alias | grep --color=never "env_" | awk '{ $1 = "" ; print }' | column -t
+echo "env_vars_file_remplace"
+
+# Load Vars on Login
+CHECK_ALIAS=$(grep "$ENV_REPO_PATH/$ENV_SECRETS_FILE" /home/$ENV_USER/.bashrc)
+if [ -z "$CHECK_ALIAS" ] ; then
+    echo ". $ENV_REPO_PATH/$ENV_SECRETS_FILE" >> /home/$ENV_USER/.bashrc
+fi
 
 # Remplace files
-env_vars_files() {
-    grep "ENV_" $ENV_REPO_PATH . -R | while read file; do env_vars() $file; done
-}
-env_vars() {
-    FILE=$1
-    WITHOUT_EXT_FILE=$(echo $FILE | cut -f1 -d ".")
-    EXT_FILE=$(echo $FILE | cut -f2 -d ".")
-    envsubst < $path > "$WITHOUT_EXT_FILE-secrets-.$EXT_FILE"
+env_vars_file_remplace() {
+    FILES_WITH_SECRETS=$(grep -sl "ENV_" $ENV_REPO_PATH . -R | grep -v "$ENV_SECRETS_FILE")
+    for file in $FILES_WITH_SECRETS ; do
+        echo "$file"
+        env_vars_file $file
+    done
 }
 
-# Usage
-echo "usage: envsubst < file_with_env_vars.yml | command -"
+env_vars_file() {
+    FILE=$1
+    EXT_REMOVE=$(echo $FILE | cut -f1 -d ".")
+    EXT_FILE=$(echo $FILE | cut -f2 -d ".")
+    envsubst < $FILE > "$EXT_REMOVE-env-vars-secrets.$EXT_FILE"
+}
