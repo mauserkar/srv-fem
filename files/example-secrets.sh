235b400e2d360c198aa31af73f3643a72fca977b
Carlos Garcia <carlos.garcia@scrm.eu>
2020-11-27 12:55:39 +0100

    update vars

--- files/example-secrets.sh
+++ files/example-secrets.sh
@@ -5,15 +5,13 @@ export ENV_USER="$USER"
 export ENV_REPO_PATH="/home/$ENV_USER/repos/srv-fem"
 # Mysql
 export ENV_MYSQL_SERVER=""
-export ENV_MYSQL_DB_DEV=""
-export ENV_MYSQL_DB_PRO=""
 export ENV_MYSQL_USER=""
 export ENV_MYSQL_PASSWORD=""
 # Wordpress
-export ENV_WORDPRESS_DB_HOST=""
-export ENV_WORDPRESS_DB_NAME=""
-export ENV_WORDPRESS_DB_USER=""
-export ENV_WORDPRESS_DB_PASSWORD=""
+export ENV_WORDPRESS_DB_DEV=""
+export ENV_WORDPRESS_DB_PRD=""
+export ENV_WORDPRESS_USER=""
+export ENV_WORDPRESS_PASSWORD=""
 ############################################################
 ###################### Alias ###############################
 alias env-vars="source /home/$USER/repos/srv-fem/files/secrets.sh"
